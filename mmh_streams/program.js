b73561a2733c65448695dba1e636c4cb0a28e689
rajveer <rajveer.kaurgill93@gmail.com>
2017-11-25 06:48:19 +0000

    you commit

---
+++ mmh_streams/program.js
@@ -0,0 +1,26 @@
+let Fs = require('fs');
+let Hapi = require('hapi');
+let Path = require('path');
+let Rot13 = require('rot13-transform');
+
+let server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+        handler: (request, reply) => {
+            var thisfile = Fs.createReadStream(Path.join(__dirname, 'input.txt'));
+            reply(thisfile.pipe(Rot13()));
+        }
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+});
